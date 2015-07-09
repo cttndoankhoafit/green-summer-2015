9df915da141723b96e61090f79e7efb02b19d17b
Khanh Nguyen <nguyenkhanh.cantho.1@gmail.com>
2015-07-09 17:17:56 +0700

    Add the base of member listview function

--- mms/mms_webapp_v1/urls.py
+++ mms/mms_webapp_v1/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import include, url
 
 from mms_webapp_v1.views.organization_type import *
 from mms_webapp_v1.views.organization.views import *
+from mms_webapp_v1.views.member import *
 
 urlpatterns = [
+	url(r'^member/', MemberListView.as_view(), name='member_list_view_v1'),
 	url(r'^organization_type/create/', OrganizationTypeCreateView.as_view(), name='organization_type_create_v1'),
-	
 	url(r'^organization/(?P<organization_id>[0-9]+)/organization/$', ChildOrganizationListView.as_view(), name='ChildOrganizationListView'),
-
 	url(r'^user/(?P<user_id>[0-9]+)/organization/$', ManagedOrganizationListView.as_view(), name='ManagedOrganizationListView'),
+	
 ]
---
+++ mms/mms_webapp_v1/views/member.py
@@ -0,0 +1,10 @@
+from django.views.generic import ListView
+
+from mms_backoffice.models import Member
+
+class MemberListView(ListView):
+	template_name = 'temporary/member/list.html'
+	paginate_by = '10'
+
+	def get_queryset(self):
+		return Member.objects.all()
