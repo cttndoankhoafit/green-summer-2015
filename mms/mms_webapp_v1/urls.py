6ec423d4722752b325cb9150f8d6501011e87ebb
Bui Trung Hai <bthai.1995@gmail.com>
2015-07-10 10:16:11 +0700

    Add OrganizationMemberListView

--- mms/mms_webapp_v1/urls.py
+++ mms/mms_webapp_v1/urls.py
@@ -4,6 +4,7 @@ from mms_webapp_v1.views.organization_type import *
 from mms_webapp_v1.views.organization import *
 from mms_webapp_v1.views.organization_temporary.views import *
 from mms_webapp_v1.views.member import *
+from mms_webapp_v1.views.member_temporary.views import *
 
 urlpatterns = [
 	url(r'^member/', MemberListView.as_view(), name='member_list_view_v1'),
@@ -11,4 +12,5 @@ urlpatterns = [
 	url(r'^organization_type/create/', OrganizationTypeCreateView.as_view(), name='organization_type_create_view_v1'),
 	url(r'^organization/(?P<organization_id>\d+)/under_organization/$', OrganizationOrganizationView.as_view(), name='organization_organization_view_v1'),
 	url(r'^user/(?P<user_id>[0-9]+)/organization/$', UserOrganizationView.as_view(), name='user_organization_view_v1'),
+	url(r'^organization/(?P<organization_id>\d+)/member/$', OrganizationMemberListView.as_view(), name='organization_member_list_view'),
 ]
